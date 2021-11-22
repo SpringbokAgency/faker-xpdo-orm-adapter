5d8970dc09c4bb43347b65fa4977aa193e2b853d
Joshua Lückers <joshualuckers@me.com>
2021-11-22 11:17:44 +0100

    Add tests workflow

--- tests/properties.sample.inc.php
+++ tests/properties.sample.inc.php
@@ -2,8 +2,8 @@
 
 /* driver-specific connection properties */
 /* mysql */
-$properties['mysql_string_dsn_test'] = 'mysql:host=localhost;dbname=faker_xpdo_test;charset=utf8';
-$properties['mysql_string_username'] = '';
+$properties['mysql_string_dsn_test'] = 'mysql:host=127.0.0.1;dbname=faker_xpdo_test;charset=utf8';
+$properties['mysql_string_username'] = 'root';
 $properties['mysql_string_password'] = '';
 
 /* PHPUnit test config */
